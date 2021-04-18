b133edc1e3bc358b6dcc97fa04abc82481853765
Andika Wasisto <andika@wasisto.com>
2021-04-18 12:28:18 +0700

    Clean code

--- app/src/test/java/com/wasisto/camrnghttp/MainCoroutineRule.kt
+++ app/src/test/java/com/wasisto/camrnghttp/MainCoroutineRule.kt
@@ -26,9 +26,10 @@ import org.junit.rules.TestWatcher
 import org.junit.runner.Description
 
 @ExperimentalCoroutinesApi
-class MainCoroutineRule(val dispatcher: TestCoroutineDispatcher = TestCoroutineDispatcher()):
-    TestWatcher(),
-    TestCoroutineScope by TestCoroutineScope(dispatcher) {
+class MainCoroutineRule(
+    val dispatcher: TestCoroutineDispatcher = TestCoroutineDispatcher()
+): TestWatcher(), TestCoroutineScope by TestCoroutineScope(dispatcher) {
+
     override fun starting(description: Description?) {
         super.starting(description)
         Dispatchers.setMain(dispatcher)
